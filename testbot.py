f0c84f6b276830a9631b840b78520f63db4b82a6
Seth Raymond <seth.raymond@maine.edu>
2017-06-06 19:39:19 -0400

    Update testbot.py
    tabs -> spaces

--- testbot.py
+++ testbot.py
@@ -1,10 +1,10 @@
 from disco.bot import Bot, Plugin
 
 class SimplePlugin(Plugin):
-	@Plugin.command('ping')
-	def on_ping_command(self, event):
-		event.msg.reply('Pong!')
+    @Plugin.command('ping')
+    def on_ping_command(self, event):
+        event.msg.reply('Pong!')
 
-	@Plugin.command('echo', '<content:str...>')
-	def on_echo_command(self, event, content):
-		event.msg.reply(content)
+    @Plugin.command('echo', '<content:str...>')
+    def on_echo_command(self, event, content):
+        event.msg.reply(content)
