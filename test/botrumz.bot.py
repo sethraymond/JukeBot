c4a20c79d13d86f5013675a6b29136b596625e44
matrumz <matrumz@hotmail.com>
2017-06-06 22:14:12 -0400

    botrumz command prefix changed
    Changed to '~'
    
    .vscode/ added to gitignore

--- test/botrumz.bot.py
+++ test/botrumz.bot.py
@@ -2,7 +2,7 @@ import discord
 import asyncio
 
 tokenFile = open("../tokens/bots/botrumz", "r")
-token = tokenFile.readline()
+token = tokenFile.readline().strip()
 
 client = discord.Client()
 
@@ -15,15 +15,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    if message.content.startswith('~test'):
         counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        tmp = await client.send_message(message.channel, 'botrumz is calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
-
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
+
+    elif message.content.startswith('~sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
 
