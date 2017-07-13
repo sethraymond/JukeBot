d7ccdefcbcfb4f182ee0c20fab432cf52ae901ff
matrumz <matrumz@hotmail.com>
2017-07-13 19:42:24 -0400

    Jukebot base discord online

--- src/JukeBot.bot.py
+++ src/JukeBot.bot.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
 import json
 import asyncio
-from libs.DiscordController import DiscordController
+from libs.DiscordController import *
 
 with open("../secrets/JukeBot.secret.json", "r") as secretsFile:
     secrets = json.load(secretsFile)
 
-discord_controller = DiscordController(secrets["botToken"], None)
-
-discord_controller.run()
+run_discord(secrets["botToken"])
--- src/libs/DiscordController.py
+++ src/libs/DiscordController.py
@@ -1,38 +1,34 @@
 import discord
-import asyncio
 
-class DiscordController(object):
+# Private Variables
+_voice_channel = None
+_command_callback = None
 
-    # Private Variables
-    _voice_channel = None
-    _bot_token = None
-    _command_callback = None
+# Public Variables
+client = discord.Client()
+discord_connected = False
 
-    # Public Variables
-    client = discord.Client()
-    connected = False
-
-    def __init__(self, bot_token, command_callback):
-        # self.client = discord.Client()
-        self._bot_token = bot_token
-        self._command_callback = command_callback if command_callback else self._default_callback
-        # if (command_callback):
-        #     self._command_callback = command_callback
-        # else:
-        #     self
+def discord_init():
+    # self._command_callback = command_callback if command_callback else self._default_callback
+    # if (command_callback):
+    #     self._command_callback = command_callback
+    # else:
+    #     self
+    pass
 
 
-    def _default_callback(self, command="", args=[], source=None):
-        pass
 
-    def run(self):
-        self.client.run(self._bot_token)
+def _default_callback(self, command="", args=[], source=None):
+    pass
 
-    @client.event
-    async def on_ready():
-        print("Discord login successful: " + self.client.user.name)
-        self.connected = True
+def run_discord(bot_token):
+    client.run(bot_token)
 
-    @client.event
-    async def on_message(message):
-        print(message.content)
+@client.event
+async def on_ready():
+    print("Discord login successful: " + client.user.name)
+    discord_connected = True
+
+@client.event
+async def on_message(message):
+    print(message.content)
