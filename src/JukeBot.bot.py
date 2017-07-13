c14140de2127b5e527f738fa13bb5ed54b29c1d5
Seth Raymond <seth.c.raymond@gmail.com>
2017-07-13 19:41:18 -0400

    Adding initial Google Play Music support to JukeBot

--- src/JukeBot.bot.py
+++ src/JukeBot.bot.py
@@ -2,11 +2,17 @@
 
 import json
 import asyncio
+import libs.GooglePlayMusicController as GPMController
 from libs.DiscordController import DiscordController
 
 with open("../secrets/JukeBot.secret.json", "r") as secretsFile:
     secrets = json.load(secretsFile)
 
-discord_controller = DiscordController(secrets["botToken"], None)
+#discord_controller = DiscordController(secrets["botToken"], None)
 
-discord_controller.run()
+#discord_controller.run()
+
+gpm = GPMController.GpmSession(secrets["gPlayAppUser"], secrets["gPlayAppPass"])
+while not gpm.logged_in:
+	gpm = GPMController.GpmSession()
+
